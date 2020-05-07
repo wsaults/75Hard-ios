a046041c817c2990e846f6642830fd52445eba50
Will Saults <will@saults.io>
2020-05-07 10:29:57 -0500

    Created project

---
+++ SeventyFiveHard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeventyFiveHard
+//
+//  Created by Will Saults on 5/7/20.
+//  Copyright © 2020 Will Saults. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
